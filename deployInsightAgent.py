eea6b47ec4756788d0a947d6d66cbf21612894f7
Ravishankar <rchandh@ncsu.edu>
2016-05-18 14:21:13 -0400

    Cleanup downloaded files after completion of deployment:

--- deployInsightAgent.py
+++ deployInsightAgent.py
@@ -27,6 +27,12 @@ def get_args():
     agentType = args.AGENT_TYPE
     return user, userInsightfinder, licenseKey, samplingInterval, reportingInterval, agentType
 
+downloadFiles = ["installInsightAgent.py", "startcron.py", "checkpackages.py", "get-pip.py"]
+
+def clearDownloads():
+    for eachFile in downloadFiles:
+        proc = subprocess.Popen("rm "+eachFile, cwd=homepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        (out,err) = proc.communicate()
 
 if __name__ == '__main__':
     global user
@@ -40,26 +46,12 @@ if __name__ == '__main__':
     global agentType
 
     homepath = os.getcwd()
-    proc = subprocess.Popen("wget --no-check-certificate https://raw.githubusercontent.com/insightfinder/InsightAgent/testing/installInsightAgent.py", cwd=homepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    (out,err) = proc.communicate()
-    if "failed" in str(err) or "ERROR" in str(err):
-        sys.exit(err)
-    proc = subprocess.Popen("wget --no-check-certificate https://raw.githubusercontent.com/insightfinder/InsightAgent/testing/startcron.py", cwd=homepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    (out,err) = proc.communicate()
-    if "failed" in str(err) or "ERROR" in str(err):
-        sys.exit(err)
-    proc = subprocess.Popen("wget --no-check-certificate https://raw.githubusercontent.com/insightfinder/InsightAgent/testing/checkpackages.py", cwd=homepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    (out,err) = proc.communicate()
-    if "failed" in str(err) or "ERROR" in str(err):
-        sys.exit(err)
-    proc = subprocess.Popen("wget --no-check-certificate https://raw.githubusercontent.com/insightfinder/InsightAgent/testing/get-pip.py", cwd=homepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    (out,err) = proc.communicate()
-    if "failed" in str(err) or "ERROR" in str(err):
-        sys.exit(err)
-    os.chmod("installInsightAgent.py",0755)
-    os.chmod("startcron.py",0755)
-    os.chmod("checkpackages.py",0755)
-    os.chmod("get-pip.py",0755)
+    for eachFile in downloadFiles:
+        proc = subprocess.Popen("wget --no-check-certificate https://raw.githubusercontent.com/insightfinder/InsightAgent/testing/"+eachFile, cwd=homepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        (out,err) = proc.communicate()
+        if "failed" in str(err) or "ERROR" in str(err):
+            sys.exit(err)
+        os.chmod(eachFile,0755)
 
     #Check if required packages are installed
     proc = subprocess.Popen(["sudo python "+os.path.join(homepath,"checkpackages.py")], cwd=homepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -98,3 +90,4 @@ if __name__ == '__main__':
     proc = subprocess.Popen([os.path.join(homepath,"startcron.py")+" -n "+user+" -u "+userInsightfinder+" -k "+licenseKey+" -s "+samplingInterval+" -r "+reportingInterval+" -t "+agentType+" -p "+password], cwd=homepath, stdout=subprocess.PIPE, shell=True)
     (out,err) = proc.communicate()
     print out
+    clearDownloads()
