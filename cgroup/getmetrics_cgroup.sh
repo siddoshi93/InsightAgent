bc95fee2d19b91e355bb30f6550c1515b7b0317c
Ravishankar <rchandh@ncsu.edu>
2016-06-06 16:15:26 -0400

    Removing sudo:

--- cgroup/getmetrics_cgroup.sh
+++ cgroup/getmetrics_cgroup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DATADIR='data/'
-cd ../$DATADIR
+cd $DATADIR
 echo $1
 while [ "$1" != "" ]; do
 	SEARCHKEY=$1
@@ -11,15 +11,15 @@ echo $SEARCHKEY
 for SEARCHKEY in "rubis_apache" "rubis_db"
 do
 echo $SEARCHKEY
-apacheweb=$(sudo docker ps --no-trunc | grep $SEARCHKEY | awk '{print $1}')
+apacheweb=$(docker ps --no-trunc | grep $SEARCHKEY | awk '{print $1}')
 echo $apacheweb
-CONTAINER_PID=`sudo docker inspect -f '{{ .State.Pid }}' $apacheweb`
+CONTAINER_PID=`docker inspect -f '{{ .State.Pid }}' $apacheweb`
 date +%s%3N | awk '{print "timestamp="$1}' > timestamp.txt & PID1=$!
-sudo cat /cgroup/memory/docker/$apacheweb/memory.usage_in_bytes | awk '{print "MemUsed="$1}' > memmetrics_$SEARCHKEY.txt & PID2=$!
+cat /cgroup/memory/docker/$apacheweb/memory.usage_in_bytes | awk '{print "MemUsed="$1}' > memmetrics_$SEARCHKEY.txt & PID2=$!
 
-sudo cat /cgroup/blkio/docker/$apacheweb/blkio.throttle.io_service_bytes | grep Read | awk '{if(NR!=1){readbytes+=$3}} END{print "DiskRead="readbytes}' > diskmetricsread_$SEARCHKEY.txt & PID3=$!
-sudo cat /cgroup/blkio/docker/$apacheweb/blkio.throttle.io_service_bytes | grep Write | awk '{if(NR!=1){writebytes+=$3;}} END{print "DiskWrite="writebytes}' > diskmetricswrite_$SEARCHKEY.txt & PID4=$!
-sudo cat /proc/$CONTAINER_PID/net/dev | awk '{if(NR!=1 && NR!=2){if($1!="lo:"){rxbytes+=$2;txbytes+=$10;}}} END{print "NetworkIn="rxbytes; print "NetworkOut="txbytes}' > networkmetrics_$SEARCHKEY.txt & PID5=$!
+cat /cgroup/blkio/docker/$apacheweb/blkio.throttle.io_service_bytes | grep Read | awk '{if(NR!=1){readbytes+=$3}} END{print "DiskRead="readbytes}' > diskmetricsread_$SEARCHKEY.txt & PID3=$!
+cat /cgroup/blkio/docker/$apacheweb/blkio.throttle.io_service_bytes | grep Write | awk '{if(NR!=1){writebytes+=$3;}} END{print "DiskWrite="writebytes}' > diskmetricswrite_$SEARCHKEY.txt & PID4=$!
+cat /proc/$CONTAINER_PID/net/dev | awk '{if(NR!=1 && NR!=2){if($1!="lo:"){rxbytes+=$2;txbytes+=$10;}}} END{print "NetworkIn="rxbytes; print "NetworkOut="txbytes}' > networkmetrics_$SEARCHKEY.txt & PID5=$!
 
 wait $PID1
 wait $PID2
