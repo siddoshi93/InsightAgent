48f327c013f098f472eb1b6aa53791fa7a8692d6
Ravishankar <rchandh@ncsu.edu>
2016-06-23 18:12:16 -0400

    Use username for path

--- checkpackages.py
+++ checkpackages.py
@@ -8,6 +8,7 @@ required_packages = ["requests","paramiko"]
 This script checks for required packages and installs using pip
 '''
 homepath = os.getcwd()
+user = os.getlogin()
 try:
     import pip
 except ImportError as e:
@@ -23,7 +24,7 @@ pyVersion = sys.version
 versionElements = pyVersion.split(" ")[0].split(".")
 version = versionElements[0] + "." + versionElements[1]
 command = "pip install --user virtualenv\n \
-        python  ~/.local/lib/python"+version+"/site-packages/virtualenv.py pyenv\n \
+        python  /home/"+user+"/.local/lib/python"+version+"/site-packages/virtualenv.py pyenv\n \
         source pyenv/bin/activate\n \
         pip install -r requirements\n"
 proc = subprocess.Popen([command], cwd=homepath, stdout=subprocess.PIPE, shell=True)
