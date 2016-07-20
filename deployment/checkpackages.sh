775377f7f9c37696b55a5c5a612c0ae9b7f2c490
Ravishankar <rchandh@ncsu.edu>
2016-07-20 16:07:33 -0400

    Correct typo

--- deployment/checkpackages.sh
+++ deployment/checkpackages.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py --force-reinstall --user
 wget --no-check-certificate https://raw.githubusercontent.com/insightfinder/InsightAgent/master/deployment/requirements
+sudo chown -R $USER /home/$USER/.local
 pip install -U --force-reinstall --user virtualenv
 version=`python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))'`
 python  /home/$USER/.local/lib/python$version/site-packages/virtualenv.py pyenv
