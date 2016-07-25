2eb3a62274e31833a387dc6b54b3440939697cee
Ravishankar <rchandh@ncsu.edu>
2016-07-25 14:11:33 -0400

    Seperate scripts for stopping hypervisor agent

---
+++ hypervisor/stopcron.sh
@@ -0,0 +1,42 @@
+
+
+usage()
+{
+	echo "Usage: ./stopcron.sh -t AGENT_TYPE
+AGENT_TYPE = hypervisor"
+}
+
+if [ "$#" -lt 2 ]; then
+	usage
+	exit 1
+fi
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-t )	shift
+			AGENT_TYPE=$1
+			;;
+		* )	usage
+			exit 1
+	esac
+	shift
+done
+
+if [ $AGENT_TYPE != 'hypervisor' ]; then
+	usage
+	exit 1
+fi
+
+if [ -z "$INSIGHTAGENTDIR" ]; then
+	export INSIGHTAGENTDIR=`pwd`
+fi
+
+TEMPCRON=/var/spool/cron/crontabs/root
+sed '/InsightAgent-master/d' $TEMPCRON > $TEMPCRON
+	
+cronpid=`cat /var/run/crond.pid`
+echo "CRONPID is"
+kill $cronpid
+crond
+
+echo "Hypervisor agent stopped"
