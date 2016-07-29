47c2cdd57c0ce0f7261077b8cfba38e6d78bd835
Ravishankar <rchandh@ncsu.edu>
2016-07-29 10:04:30 -0400

    Work for both root and normal users:

--- deployment/deployInsightAgent.sh
+++ deployment/deployInsightAgent.sh
@@ -47,7 +47,7 @@ fi
 
 wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py --force-reinstall --user
 wget --no-check-certificate https://raw.githubusercontent.com/insightfinder/InsightAgent/testing/deployment/requirements
-/home/$USER/.local/bin/pip install -U --force-reinstall --user virtualenv
+~/.local/bin/pip install -U --force-reinstall --user virtualenv
 if [ "$?" -ne "0" ]; then
     echo "pip install failed. Please install the pre-requisites using the following commands and retry deployment again"
 if [ "$(command -v yum)" ]; then
